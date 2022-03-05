76e7fa59be858dbf5358f6b0cb35269e3be10bf7
leslieXin92 <91523639+leslieXin92@users.noreply.github.com>
2022-03-06 04:35:56 +0800

    动画

---
+++ 29_过度与动画/292_过渡/src/components/Hello.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <button @click="handleShow">show/hide</button>
+
+        <transition name="hello" appear>
+            <h2 v-show="isShow">hello</h2>
+        </transition>
+
+        <hr />
+
+        <transition-group name="hello" appear>
+            <h2 v-show="isShow" key="1">hello</h2>
+            <h2 v-show="isShow" key="2">hello</h2>
+        </transition-group>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'hello',
+    data () {
+        return {
+            isShow: true
+        }
+    },
+    methods: {
+        handleShow () {
+            this.isShow = !this.isShow
+        }
+    }
+}
+</script>
+
+<style>
+h2 {
+    background-color: paleturquoise;
+}
+
+/* 进入的起点、离开的重点 */
+.hello-enter,
+.hello-leave-to {
+    transform: translateX(-100%);
+}
+
+/* 进入过程、离开过程 */
+.hello-enter-active,
+.hello-leave-active {
+    transition: 0.5s linear;
+}
+
+/* 进入的重点、离开的起点 */
+.hello-enter-to,
+.hello-leave {
+    transform: translateX(0);
+}
+</style>
